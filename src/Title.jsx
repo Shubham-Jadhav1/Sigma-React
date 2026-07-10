bef33094b4ea68b351963545659ad872e1c163d6
Shubham Jadhav <jadhavshubuam413@gmail.com>
2026-07-10 17:19:11 +0530

    jsx with curly braces

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,7 +1,12 @@
 function Title() {
+  let name = "Shubham";
   return  <div>
-  <h1>I am the Title</h1>
-  <h3>I am Description</h3>
+  {/* <h1>I am the Title</h1>
+  <h3>I am Description</h3> */}
+
+  
+  <p>2 * 2 = {2 * 2}</p>
+  <p>I'm {name.toUpperCase()}</p>
   </div>
   
 }
