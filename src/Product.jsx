3d1c5b1f4c5cf6b0c7d8f85a56ad45f51c1bee01
Shubham Jadhav <jadhavshubuam413@gmail.com>
2026-07-13 17:12:34 +0530

    Styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,13 +1,25 @@
 import "./Product.css";
 
-function Product({title, price ="1"}) {
-  console.log(title);
+function Product({title, price}) {
+  // console.log(title);
+ 
+  // console.log(feature);
+  // const list = feature.map((features) => <li>{features}</li>)
+  let isDiscount = price > 30000;
+  let styles = {backgroundColor: isDiscount ? "orange" : ""};
+  
+  
     return (
-          <div className="Product">
-            <h3>{title},</h3>
-            <h5>price: {price}</h5>
+          <div className="Product" style ={styles}>
+            <h3>{title}</h3>
+            <h5>price: {price} </h5>
+            {/* {price > 30000 ? <p>"Discount of 5%" </p>: null} */}
+            {isDiscount && <p>Discount of 5%</p>}
+            {/* <p>{feature.map((features) => <li>{features}</li>)}</p> */}
+           
           </div>
     );
 }
+
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,13 +1,15 @@
 import Product from "./Product.jsx"
 
 function productTab() {
+    //  let option = ["Tech","non-Tech", "durable"];
+    //  let option2 = { a: "Tech", b: "fast"};
     return (
-        <>
+        <div className="ProductTab">
             <Product title="phone" price={30000} />
              <Product title="laptop" price={50000} />
-              <Product title="pen" />
+              <Product title="pen" price="10" />
            
-        </>
+        </div>
     );
 }
 export default productTab;
